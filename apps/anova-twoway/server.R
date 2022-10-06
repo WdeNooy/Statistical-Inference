2dab12d8e787cb2937255d98f794f8b07420289f
WdeNooy <w.denooy@uva.nl>
2022-10-06 16:36:30 +0200

    Edits to Ch. 4 & 6, improved anova-twoway app.

--- apps/anova-twoway/server.R
+++ apps/anova-twoway/server.R
@@ -25,24 +25,24 @@ shinyServer(function(input, output) {
       # Samples is dependent on the new sample button.
       
       if ( input$newsampbut == 0 ) {
-      #start data set with larger differences for endorser than for sex
-      df <- data.frame(
-        y = c(
-          6 + rnorm(1, -0.4, sd = 0.6),
-          6 + rnorm(1, -0.4, sd = 0.6),
-          6 + rnorm(1, 0.4, sd = 0.6),
-          6 + rnorm(1, 0.4, sd = 0.6),
-          7 + rnorm(1, -0.4, sd = 0.6),
-          7 + rnorm(1, -0.4, sd = 0.6),
-          7 + rnorm(1, 0.4, sd = 0.6),
-          7 + rnorm(1, 0.4, sd = 0.6),
-          3 + rnorm(1, -0.4, sd = 0.6),
-          3 + rnorm(1, -0.4, sd = 0.6),
-          3 + rnorm(1, 0.4, sd = 0.6),
-          3 + rnorm(1, 0.4, sd = 0.6)),
-        x = spacemaker(3, n, .15),
-        cat1 = rep(c("Clooney","Jolie","NoEnd"),each = n),
-        cat2 = rep(c("Men","Women"),each = 2))
+        #start data set with larger differences for endorser than for sex
+        df <- data.frame(
+          y = c(
+            6 + rnorm(1, -0.4, sd = 0.6),
+            6 + rnorm(1, -0.4, sd = 0.6),
+            6 + rnorm(1, 0.4, sd = 0.6),
+            6 + rnorm(1, 0.4, sd = 0.6),
+            7 + rnorm(1, -0.4, sd = 0.6),
+            7 + rnorm(1, -0.4, sd = 0.6),
+            7 + rnorm(1, 0.4, sd = 0.6),
+            7 + rnorm(1, 0.4, sd = 0.6),
+            3 + rnorm(1, -0.4, sd = 0.6),
+            3 + rnorm(1, -0.4, sd = 0.6),
+            3 + rnorm(1, 0.4, sd = 0.6),
+            3 + rnorm(1, 0.4, sd = 0.6)),
+          x = spacemaker(3, n, .15),
+          cat1 = rep(c("Clooney","Jolie","No Endorser"),each = n),
+          cat2 = rep(c("Men","Women"),each = 2))
       }
       
       else {
@@ -50,33 +50,33 @@ shinyServer(function(input, output) {
         # Define the means for each category.
         
         mCloon <- runif(1, min = 3, max = 7)
-      mJolie <- runif(1, min = 3, max = 7)
-      mNoEnd <- runif(1, min = 3, max = 7)
-      mMen <-   runif(1, min = -1, max = 1)
-      mWom <-   runif(1, min = -1, max = 1)
-      
-      # Generate the data frame needed for plotting the positions of each dot
-      # variable y is the mean of each endorser category mixed with a random 
-      # bit for the sex category with sd of 1. 
-      
-      df <-
-        data.frame(
-          y = c(
-            mCloon + rnorm(1, mMen, sd = 0.6),
-            mCloon + rnorm(1, mMen, sd = 0.6),
-            mCloon + rnorm(1, mWom, sd = 0.6),
-            mCloon + rnorm(1, mWom, sd = 0.6),
-            mJolie + rnorm(1, mMen, sd = 0.6),
-            mJolie + rnorm(1, mMen, sd = 0.6),
-            mJolie + rnorm(1, mWom, sd = 0.6),
-            mJolie + rnorm(1, mWom, sd = 0.6),
-            mNoEnd + rnorm(1, mMen, sd = 0.6),
-            mNoEnd + rnorm(1, mMen, sd = 0.6),
-            mNoEnd + rnorm(1, mWom, sd = 0.6),
-            mNoEnd + rnorm(1, mWom, sd = 0.6)),
-          x = spacemaker(3, n, .15),
-          cat1 = rep(c("Clooney","Jolie","NoEnd"),each = n),
-          cat2 = rep(c("Men","Women"),each = 2))
+        mJolie <- runif(1, min = 3, max = 7)
+        mNoEnd <- runif(1, min = 3, max = 7)
+        mMen <-   runif(1, min = -1, max = 1)
+        mWom <-   runif(1, min = -1, max = 1)
+        
+        # Generate the data frame needed for plotting the positions of each dot
+        # variable y is the mean of each endorser category mixed with a random 
+        # bit for the sex category with sd of 1. 
+        
+        df <-
+          data.frame(
+            y = c(
+              mCloon + rnorm(1, mMen, sd = 0.6),
+              mCloon + rnorm(1, mMen, sd = 0.6),
+              mCloon + rnorm(1, mWom, sd = 0.6),
+              mCloon + rnorm(1, mWom, sd = 0.6),
+              mJolie + rnorm(1, mMen, sd = 0.6),
+              mJolie + rnorm(1, mMen, sd = 0.6),
+              mJolie + rnorm(1, mWom, sd = 0.6),
+              mJolie + rnorm(1, mWom, sd = 0.6),
+              mNoEnd + rnorm(1, mMen, sd = 0.6),
+              mNoEnd + rnorm(1, mMen, sd = 0.6),
+              mNoEnd + rnorm(1, mWom, sd = 0.6),
+              mNoEnd + rnorm(1, mWom, sd = 0.6)),
+            x = spacemaker(3, n, .15),
+            cat1 = rep(c("Clooney","Jolie","No Endorser"),each = n),
+            cat2 = rep(c("Men","Women"),each = 2))
       }  
       return(df)
     })
@@ -92,22 +92,21 @@ shinyServer(function(input, output) {
       ggplot(df, aes(x = x, y = y)) +
       
       geom_line(y = mean(df$y),
-                aes(linetype = "Grand Mean", colour = "Grand Mean"),
+                aes(linetype = "Grand Mean", colour = "black"),
                 size = 1.3) +
       geom_point(size = 5, aes(fill = cat1, shape = cat2)) +
       scale_colour_manual(values = 
-                            c("Grand Mean" = "black",
-                            "Clooney" = unname(brewercolors["Orange"]),
-                            "Jolie" = unname(brewercolors["Blue"]),
-                            "No Endorser" = unname(brewercolors["Green"]))) +
+                            c("Clooney" = unname(brewercolors["Orange"]),
+                              "Jolie" = unname(brewercolors["Blue"]),
+                              "No Endorser" = unname(brewercolors["Green"]))) +
       scale_fill_manual(values = 
                           c("Clooney" = unname(brewercolors["Orange"]),
                             "Jolie" = unname(brewercolors["Blue"]),
-                            "NoEnd" = unname(brewercolors["Green"]))) +
+                            "No Endorser" = unname(brewercolors["Green"]))) +
       scale_shape_manual(values = c("Men" = 21, "Women" = 22)) +
-      guides(colour = guide_legend(title = "Means:"),
+      guides(colour = "none", # guide_legend(title = "Means:"),
              linetype = guide_legend(title = "Means:"),
-             fill = FALSE,
+             fill = "none",
              shape = guide_legend(title = "Sex:")) +
       scale_x_continuous(breaks = 1:3,
                          labels = c("Clooney", "Jolie", "No Endorser")) +
@@ -120,7 +119,7 @@ shinyServer(function(input, output) {
     # Plot With only Endorser ----
     
     if ("1" %in% input$groupselect & length(input$groupselect) == 1)
-      {
+    {
       
       # Create data frame for arrows between grand mean and group means.
       
@@ -130,7 +129,7 @@ shinyServer(function(input, output) {
         y.grpgr    = rep(mean(df$y), length(df$y)),
         yend.grpgr = 
           rep(as.vector(by(df$y, df$cat1, mean, simplify = TRUE)), each =  4))
-
+      
       p <-
         p +
         geom_segment( x    = min(subset(df, cat1 == "Clooney")$x - .09),
@@ -138,19 +137,19 @@ shinyServer(function(input, output) {
                       y    = mean(subset(df, cat1 == "Clooney")$y),
                       yend = mean(subset(df, cat1 == "Clooney")$y),
                       size = 1.2,
-                      aes(colour = "Clooney",linetype = "Clooney")) +
+                      aes(colour = "Clooney",linetype = "Grand Mean")) +
         geom_segment( x    = min(subset(df, cat1 == "Jolie")$x - .09),
                       xend = max(subset(df, cat1 == "Jolie")$x + .09),
                       y    = mean(subset(df, cat1 == "Jolie")$y),
                       yend = mean(subset(df, cat1 == "Jolie")$y),
                       size = 1.2,
-                      aes(colour = "Jolie",linetype = "Jolie")) +
-        geom_segment( x    = min(subset(df, cat1 == "NoEnd")$x - .09),
-                      xend = max(subset(df, cat1 == "NoEnd")$x + .09),
-                      y    = mean(subset(df, cat1 == "NoEnd")$y),
-                      yend = mean(subset(df, cat1 == "NoEnd")$y),
+                      aes(colour = "Jolie",linetype = "Grand Mean")) +
+        geom_segment( x    = min(subset(df, cat1 == "No Endorser")$x - .09),
+                      xend = max(subset(df, cat1 == "No Endorser")$x + .09),
+                      y    = mean(subset(df, cat1 == "No Endorser")$y),
+                      yend = mean(subset(df, cat1 == "No Endorser")$y),
                       size = 1.2,
-                      aes(colour = "No Endorser",linetype = "No Endorser"))  +
+                      aes(colour = "No Endorser",linetype = "Grand Mean"))  +
         geom_segment( x        = dfarrows$x.grpgr,
                       xend     = dfarrows$xend.grpgr,
                       y        = dfarrows$y.grpgr,
@@ -161,18 +160,20 @@ shinyServer(function(input, output) {
                         arrow(length = unit(2, "mm"),
                               ends   = "both",
                               type   = "closed")) + 
-        scale_linetype_manual( values = c("Men"         = "dotted",
-                                          "Women"       = "dashed",
-                                          "Grand Mean"  = "solid",
-                                          "Clooney"     = "solid",
-                                          "Jolie"       = "solid",
-                                          "No Endorser" = "solid")) 
+        guides(colour = guide_legend(title = ""),
+               linetype = guide_legend(title = "Means:"),
+               fill = "none",
+               shape = "none") + # guide_legend(title = "Sex:")) # +
+        scale_linetype_manual( values = c("Grand Mean"  = "solid")) #,
+                                          # "Clooney"     = "solid",
+                                          # "Jolie"       = "solid",
+                                          # "No Endorser" = "solid")) 
     }
     
     #Plot With Only Sex Selected ----
     
     if("2" %in% input$groupselect & length(input$groupselect) == 1)
-      {
+    {
       
       # Data frame for arrows between grand and group means.
       
@@ -182,7 +183,7 @@ shinyServer(function(input, output) {
                     y.grpgr    = rep(mean(df$y), length(df$y)),
                     yend.grpgr = 
                       rep(as.vector(by(df$y, df$cat2, mean, simplify = TRUE)),3, each = 2)
-                    )
+        )
       
       p <-
         p +
@@ -196,19 +197,19 @@ shinyServer(function(input, output) {
                       y    = mean(subset(df, cat2 == "Women")$y),
                       yend = mean(subset(df, cat2 == "Women")$y),
                       aes(linetype = "Women")) +
-        scale_linetype_manual(values = c("Men"        = "dotted",
-                                         "Women"      = "dashed",
-                                         "Grand Mean" = "solid")) +
-        guides( colour       = FALSE,
+        scale_linetype_manual(values = c("Grand Mean" = "solid",
+                                         "Men"        = "dotted",
+                                         "Women"      = "dashed")) +
+        guides( colour       = "none",
                 linetype     = guide_legend(title = "Means:",
                                             order = 2,
                                             override.aes = list(
                                               linetype = 
                                                 c("solid", "dotted", "dashed"),
-                                                colour = 
+                                              colour = 
                                                 c("black", "black", "black"),
-                                                size = 0.5)),
-                fill         = FALSE,
+                                              size = 0.5)),
+                fill         = "none",
                 shape        = guide_legend(title = "Sex:", order = 1)) +
         geom_segment( x        = dfarrows$x.grpgr,
                       xend     = dfarrows$xend.grpgr,
@@ -224,8 +225,8 @@ shinyServer(function(input, output) {
     # Plot With Endorser And Sex Selected ----
     
     if(length(input$groupselect) == 2)
-      {
-
+    {
+      
       meandf <-
         data.frame(
           x    = as.vector(by(df$x,INDICES = list(df$cat1,df$cat2),min) - 0.1), 
@@ -233,7 +234,7 @@ shinyServer(function(input, output) {
           y    = as.vector(by(df$y,INDICES = list(df$cat1,df$cat2),mean)),
           cat1 = unique(df$cat1),
           cat2 = rep(unique(df$cat2),1,each = 3))
-
+      
       meandf <- meandf[order(meandf$cat1,meandf$cat2),]
       
       dfarrows <-
@@ -252,21 +253,13 @@ shinyServer(function(input, output) {
                          colour   = cat1,
                          linetype =  cat2),
                      size = 1) +
-        scale_linetype_manual(values = c("Men" = "dotted",
-                                         "Women" = "dashed",
-                                         "Grand Mean" = "solid")) +
-        guides(linetype = guide_legend(
-                            title = "Means:",
-                            override.aes = list(
-                                            linetype = 
-                                              c("solid", "dotted", "dashed"),
-                                            colour = 
-                                              c("black", "black", "black"),
-                                            size = 0.5),
-                           order = 2),
-                           colour = FALSE,
-                           fill = FALSE,
-                           shape = guide_legend(title = "Sex", order = 1)) +
+        scale_linetype_manual(values = c("Grand Mean" = "solid",
+                                         "Men" = "dotted",
+                                         "Women" = "dashed")) +
+        guides(colour = guide_legend(title = "Means:"),
+               linetype = guide_legend(title = ""),
+               fill = "none",
+               shape = "none") + # guide_legend(title = "Sex:")) +
         geom_segment(x = dfarrows$x.grpgr,
                      xend = dfarrows$xend.grpgr,
                      y = dfarrows$y.grpgr,
@@ -274,13 +267,13 @@ shinyServer(function(input, output) {
                      linetype = "solid",
                      colour   = "grey",
                      arrow = arrow(
-                      length = unit(2, "mm"),
-                      ends = "both",
-                      type = "closed"))
+                       length = unit(2, "mm"),
+                       ends = "both",
+                       type = "closed"))
     }
-   
+    
     # Print the plot 
     p
-  
-})
+    
+  })
 })
